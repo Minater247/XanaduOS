d4692a320cd3bcb49fdb73bbca2cb5215d75d084
Nathan Reed <nreed132@gmail.com>
2022-06-15 20:45:21 -0700

    Change name to Xanadu, add newline to tty.c

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/xanaduos.kernel isodir/boot/xanaduos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "xanaduos" {
+	multiboot /boot/xanaduos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o xanaduos.iso isodir
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -5,4 +5,5 @@
 void kernel_main(void) {
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
+	printf("This should print on line 2.\n");
 }
